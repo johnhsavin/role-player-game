5a43f73c147f892c131dcf8afeffe928f81d290f
John Savin <johnsavin@proton.me>
2023-12-23 11:48:04 -0500

    added goFight function

--- script.js
+++ script.js
@@ -147,7 +147,10 @@ function fightSlime() {
   
   function goFight() {
     update(locations[3]);
-  
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
   }
   
   function attack() {
