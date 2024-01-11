d1277f88c952e1c3ef07342a10f27dca14e2b002
John Savin <johnsavin@proton.me>
2024-01-11 11:15:51 -0500

    additional javascript functions for fighting

--- script.js
+++ script.js
@@ -69,6 +69,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You die. ☠️",
+      },
+      {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! 🎉",
       }
 ];
 
@@ -169,20 +175,16 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-    health -= monsters[fighting].level;
+    health -= getMonsterSttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
     if (health <= 0) {
         lose();
       } else if (monsterHealth <= 0) {
-        if (fighting === 2) {
-          winGame();
-        } else {
-          defeatMonster();
+        fighting === 2 ? winGame() : defeatMonster();
         }
       }
-    }
     
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
@@ -193,32 +195,16 @@ function defeatMonster() {
     xp += monsters[fighting].level;
     goldText.innerText = gold;
     xpText.innerText = xp;
-    update(locations[4]); F
+    update(locations[4]); 
 }
 
 function lose() {
     update(locations[5]);
 }
-function restart() {
-    xp = 0;
-    health = 100;
-    gold = 50;
-    currentWeapon = 0;
-    inventory = ["stick"];
 
+function winGame() {
+update(locations[6]);
 }
-
-function restart() {
-    xp = 0;
-    health = 100;
-    gold = 50;
-    currentWeapon = 0;
-    inventory = ["stick"];
-    goldText.innerText =
-    healthText.innerText = 
-    xpText.innerText = 
-    goTown();
-  }
 
   function restart() {
     xp = 0;
